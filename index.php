91ddb0aaf5e33f8ec6ca74032f5f651b44111356
claudiaarciniegas <claudiaaarciniegas83@gmail.com>
2021-05-04 17:22:56 -0500

    Condiciones_if_else_eslif_switch_04_05_2021

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+    
+    //Condicionales
+
+    // $num1=(int) 5;
+    // $num2=(int) 4;
+
+    // if($num1 >0 && $num1 <=5){
+    //     echo <<<NUMEROMAYORA0
+    //     El numero $num1 es mayor a 0
+    // NUMEROMAYORA0;
+    //     }elseif($num1==0){
+    //         echo <<<NumeroEsIguala0
+    //         El numero $num1 es igual a 0
+    // NumeroEsIguala0;
+    //     }elseif($num1<0 && $num1 >= -5){
+    //         echo <<<NumeroesNegativo
+    //         El numero $num1 es negativo
+    // NumeroesNegativo;
+    //     }else{
+    //         echo <<<ProbabilidadNoExistente
+    //         El numero $num1 no existente
+    // ProbabilidadNoExistente;
+    //     }
+
+
+
+
+    // $num1=(int) 5;
+    // $num2=(int) 10;
+    // $result=(int) 0;
+    
+    // var_dump((++$num1 || $num2) && !(--$num2 == !(num1+2)));
+    //         ((  6     ||  10) &&   !(  9     == !(8))
+    //         (        true     &&   !(  true  == !(true))     )
+    //         (        true     &&   !(  true  == false))
+    //         (        true     &&   !(false))
+    //         (        true     &&    true)
+                        
+    //var_dump (! (!(true) && !($num2-=6 <= $ num1)));
+            // !(false      &&  !(4<=5))
+            // !(false      &&   !(true))
+            // !(false      &&   false)
+            // !(false)
+            // true
+    //var_dump($num1 > $num2 || !(($num1+5)($result**=10) < $num2) && !(false)));
+    //          false      ||      false         &&   true
+    //              false  ||            !(false)
+    //              false  ||              true
+    //                          true  
+?>
